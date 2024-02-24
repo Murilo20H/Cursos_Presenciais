a5aed65167c18ed1db0b43efdc4dd1db0acafd67
Murilo20H <murilohenrique123correa@gmail.com>
2024-02-24 12:33:16 -0300

    Adicionando Curso Programando seu Futuro

--- Programando_seu_Futuro/Monitor_2024/Modulo1/Aula04/Exercicio04/Program.cs
+++ Programando_seu_Futuro/Monitor_2024/Modulo1/Aula04/Exercicio04/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args) 
     {
-        
+        Console.Write("A lampada está ligada: ");
+        bool estaLigada = bool.Parse(Console.ReadLine());
+        estaLigada = !estaLigada;
+        Console.WriteLine("A lampada está ligada: " + fazerAcao);
     }
 }
---
+++ Programando_seu_Futuro/Monitor_2024/Modulo1/Aula04/Exercicio05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        Console.Write("Digite o cheat code: ");
+        string cheatCode = Console.ReadLine();
+
+        if (cheatCode == "codigo ultra secreto")
+        {
+            Console.WriteLine("Você desbloqueou uma área secreta!");
+        }
+    }
+}
